0463337a3f4d5c06e2bf258d8350d51b47956016
Victor Duque <victor.duque11@hotmail.com>
2024-02-12 19:10:17 -0300

    PassEncoder

--- src/main/java/com/betrybe/agrix/ebytr/staff/security/SecurityConfiguration.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/security/SecurityConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -47,5 +49,10 @@ public class SecurityConfiguration {
       AuthenticationConfiguration authenticationConfiguration)
       throws Exception {
     return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
