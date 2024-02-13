4e28e9d988ce05183d99a19ff093dd167db49f48
Victor Duque <victor.duque11@hotmail.com>
2024-02-12 22:27:20 -0300

    Access limitation cop farm fertilizer

--- src/main/java/com/betrybe/agrix/ebytr/staff/controllers/FarmController.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controllers/FarmController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,6 +51,7 @@ public class FarmController {
    * @return return.
    */
   @GetMapping
+  @Secured({"ROLE_ADMIN", "ROLE_MANAGER", "ROLE_USER"})
   public ResponseEntity<List<FarmDto>> getAllFarms() {
     List<Farm> allFarms = farmService.getAllFarms();
     List<FarmDto> allFarmsDto = allFarms.stream().map(farm -> FarmDto.toDto(farm)).toList();
